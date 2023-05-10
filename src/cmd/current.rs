9c91d5d32b36e62d6f01b432e7298980596c49e0
Ayush Jha <ayush@wasmer.io>
2023-05-10 18:22:55 +0545

    New version of wasmerenv

--- src/cmd/current.rs
+++ src/cmd/current.rs
@@ -1,7 +1,16 @@
-use crate::utils::find_current_wasmer;
+use crate::utils::{find_current_wasmer, verify_wasmerenv_is_in_path, find_current_wasmer_dir};
 
-pub fn current() -> anyhow::Result<()> {
+pub fn current(verbose: bool) -> anyhow::Result<()> {
+    verify_wasmerenv_is_in_path()?;
     let current_version = find_current_wasmer();
     println!("wasmer {}", current_version.unwrap());
+
+    if !verbose {
+        return Ok(());
+    }
+    if let Some(path) = find_current_wasmer_dir()?.to_str() {
+        println!("Installed at: {}", path);
+    }
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use clap::{command, Parser, Subcommand};
-use cmd::{install::install, list::list, current::current, shell::shell};
+use cmd::{install::install, list::list, current::current, shell::shell, exec::exec};
 use semver::VersionReq;
-
+use std::{io::{self, Read}, str::FromStr};
 mod cmd;
+use std::env;
+
+
+use atty;
 mod utils;
 use anyhow::Result;
 #[derive(Parser)]
@@ -15,7 +19,10 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     /// Display the currently active version of wasmer
-    Current {},
+    Current {
+        #[arg(long, default_value = "false")]
+        verbose: bool,
+    },
 
     /// Configure wasmerenv for a specific shell (bash, zsh, fish)
     Shell {
@@ -27,6 +34,8 @@ enum Commands {
     Use {
         /// Use a specific version. Install the latest version if not specified
         version: Option<VersionReq>,
+        // #[arg(long, short)]
+        // command: Option<String>
     },
 
     /// List all the available versions of wasmer
@@ -41,20 +50,71 @@ enum Commands {
         #[arg(long, short, default_value = "false")]
         all: bool,
     },
+
+    /// Run command with wasmer
+    Exec {
+        /// Filter versions based on semver
+        #[arg(long, short)]
+        use_version: Option<VersionReq>,
+
+        /// wasmer command to run
+        command: Vec<String>
+    }
+}
+
+fn get_version_from_stdin()  -> Option<VersionReq> {
+    if atty::is(atty::Stream::Stdin) {
+        return None;
+    }
+    let mut buffer = String::new();
+    if io::stdin().read_to_string(&mut buffer).is_err() {
+        return None;
+    }
+    let stripped_buffer = buffer.strip_suffix("\n");
+    if stripped_buffer.is_none() {
+        return None;
+    }
+
+    buffer = stripped_buffer.unwrap().to_string();
+    return Some(VersionReq::from_str(&buffer).unwrap());
+}
+
+
+fn get_version_from_env() -> Option<VersionReq> {
+    match env::var("WASMER_VERSION") {
+        Ok(val) => Some(VersionReq::from_str(&val).unwrap()),
+        Err(_) => None
+    }
 }
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
+    let stdin_version = get_version_from_stdin();
+    let user_specified_version = if stdin_version.is_some() {stdin_version} else {
+        get_version_from_env()
+    };
+
     let command = cli.command;
     match command {
-        Commands::Use { version } => install(version),
+        Commands::Use { version } => {
+            let version_to_use =  if version.is_some() {version} else {user_specified_version};
+            install(version_to_use)
+        },
         Commands::List {
             version,
             count,
             all,
-        } => list(version, count, all),
-        Commands::Current {} => current(),
+        } => {
+            let version_to_use =  if version.is_some() {version} else {user_specified_version};
+            list(version_to_use, count, all)
+        },
+        Commands::Current {verbose} => current(verbose),
         Commands::Shell { name } => shell(name),
+        Commands::Exec {
+            use_version, command
+        } => {
+            exec(use_version, command)
+        }
     }
 }
