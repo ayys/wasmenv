9f7723cbc0bf40c07ddf8fd737719080b55adcfa
Ayush Jha <ayushjha@proton.me>
2024-12-16 18:27:02 +0545

    chore: specify nix packages

--- flake.nix
+++ flake.nix
@@ -11,12 +11,15 @@
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
       in
-      {
-        defaultPackage = naersk-lib.buildPackage ./.;
-        devShell = with pkgs; mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      }
+        {
+          defaultPackage = naersk-lib.buildPackage ./.;
+          packages = rec {
+            wasmenv = naersk-lib.buildPackage ./.;
+          };
+          devShell = with pkgs; mkShell {
+            buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          };
+        }
     );
 }
