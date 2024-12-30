3f9feded01250f22222fe665c49bd04f4e828779
Ayush Jha <ayushjha@protonmail.com>
2024-12-30 13:26:41 +0545

    fix: install wasmenv with flake

--- flake.nix
+++ flake.nix
@@ -17,7 +17,20 @@
             wasmenv = naersk-lib.buildPackage ./.;
           };
           devShell = with pkgs; mkShell {
-            buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rustPackages.clippy
+              openssl.dev # Add OpenSSL development libraries
+            ];
+
+            # Environment variables for `openssl-sys` crate
+            OPENSSL_DIR = "${pkgs.openssl.dev}";
+            OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+            OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+            RUSTFLAGS = "-C link-arg=-L${pkgs.openssl.out}/lib";            
             RUST_SRC_PATH = rustPlatform.rustLibSrc;
           };
         }
