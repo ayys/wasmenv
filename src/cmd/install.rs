a36905b997ee6f6e2914536b84be743b93f5cc8d
Ayush Jha <ayushjha@protonmail.com>
2025-01-02 14:52:04 +0545

    fix: make sure symlink doesn't already exist before creating it

--- src/cmd/install.rs
+++ src/cmd/install.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-use anyhow;
+use anyhow::{self, Context};
 
 use semver_eq::VersionReq;
 
@@ -27,15 +27,6 @@ fn install_version(
     install_prerelease: bool,
 ) -> anyhow::Result<Release> {
     verify_wasmenv_is_in_path()?;
-    let current_version = find_current_wasmer();
-    let match_with_current_version = current_version.is_some();
-    if match_with_current_version {
-        if let Some(ref ver) = version {
-            if ver.matches(&current_version.unwrap()) {
-                return Err(anyhow::anyhow!("You are already using wasmer {}", ver));
-            }
-        }
-    }
 
     let release = match release_to_install(&version, install_prerelease)? {
         Some(rel) => rel,
@@ -54,6 +45,7 @@ fn install_version(
     let wasmer_version_dir = data_dir.join(wasmer_dir_path);
     let wasmer_current_dir = data_dir.join("current");
     let wasmer_old_dir = data_dir.join(".wasmenv/old");
+
     if download_and_install_wasmer(&release, &wasmer_version_dir).is_err()
         && wasmer_version_dir.exists()
         && wasmer_old_dir.exists()
@@ -61,21 +53,29 @@ fn install_version(
         fs::rename(&wasmer_old_dir, &wasmer_current_dir)?;
         println!("Failed to install wasmer. Reverting back to the old version.");
     };
+
     let current_wasmer = &wasmer_current_dir.join("bin/wasmer");
     let versioned_wasmer = &wasmer_version_dir.join("bin/wasmer");
+
     if current_wasmer.exists() {
         fs::remove_file(current_wasmer)?;
     }
+
     fs::create_dir_all(&wasmer_current_dir)?;
 
     // make sure the current wasmer directory exists
-    let parent_dir = current_wasmer.parent().unwrap();
+    let parent_dir = current_wasmer
+        .parent()
+        .context("Find the parent of current wasmer dir")?;
     fs::create_dir_all(parent_dir)?;
     symlink::symlink_file(versioned_wasmer, current_wasmer)?;
-    symlink::symlink_file(
-        versioned_wasmer,
-        wasmer_current_dir.join(format!("bin/wasmer{version}")),
-    )?;
+
+    let wasmer_versioned_path = wasmer_current_dir.join(format!("bin/wasmer{version}"));
+    if wasmer_versioned_path.exists() {
+        fs::remove_file(&wasmer_versioned_path).context("Removing versioned path")?;
+    }
+
+    symlink::symlink_file(versioned_wasmer, wasmer_versioned_path)?;
     env::set_var("WASMER_DIR", &wasmer_current_dir);
     Ok(release)
 }
